57760df46913fea11e819f8a7f46f6063a038eeb
Jake Hammond <jakehammond226@gmail.com>
2022-07-15 13:55:05 -0500

    commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using StaticExercise;
+
+
+Console.WriteLine("Welcome to the temperature converter!\n");
+
+
+Console.WriteLine("Please enter in the degrees in fahrenheit that you would like to convert to celsius!");
+
+
+Console.WriteLine(TempConverter.FahrenheitToCelsius(Convert.ToDouble(Console.ReadLine())));
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Please enter in the degrees in celsius that you would like to convert to celsius!");
+
+
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(Convert.ToDouble(Console.ReadLine())));
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double Ftemp)
+        {
+
+            double celcius = 0;
+
+            celcius = (Ftemp - 32) / 1.8;
+
+            return celcius;
+
+        }
+
+
+        public static double CelsiusToFahrenheit(double Ctemp)
+        {
+
+            double fahrenheit = 0;
+
+            fahrenheit = (1.8 * Ctemp) + 32;
+
+            return fahrenheit; 
+
+        }
+}
+}
+
